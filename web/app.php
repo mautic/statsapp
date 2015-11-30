04a3e9800c82c4b2faf70106c31e44ef9988fa24
Michael Babker <mbabker@flbab.com>
2015-11-30 17:30:41 -0500

    Move back to Symfony's normal web folder structure

--- web/app.php
+++ web/app.php
@@ -14,13 +14,14 @@ date_default_timezone_set('UTC');
  * @var Composer\Autoload\ClassLoader
  */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../app/bootstrap.php.cache';
+include_once __DIR__.'/../var/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
+// Enable APC for autoloading to improve performance.
+// You should change the ApcClassLoader first argument to a unique prefix
+// in order to prevent cache key conflicts with other applications
+// also using APC.
 /*
-$apcLoader = new \Symfony\Component\ClassLoader\ApcClassLoader('sf2', $loader);
+$apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();
 $apcLoader->register(true);
 */
