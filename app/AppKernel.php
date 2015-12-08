8e0e3eafc0e15ab3c6fec712529023ba49e5f9ee
Michael Babker <mbabker@flbab.com>
2015-12-08 11:21:27 -0500

    Update all dependencies to Symfony 3.0

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -37,7 +37,6 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         // Bundles only loaded in test env
